867496245489632869f68e8495a5f0313a7852e8
Mac <macair@Macs-MacBook-Air.local>
2026-04-11 14:05:50 +0500

    Add Dockerfile, docker-compose, dockerignore and update server.js for Part I

--- student-task-manager-backend/backend/server.js
+++ student-task-manager-backend/backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const path = require("path");
 const connectDB = require("./config/db");
 
 dotenv.config();
@@ -8,27 +9,30 @@ connectDB();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (
-        !origin ||
-/^http:\/\/localhost(:\d+)?$/.test(origin) ||
-/^http:\/\/192\.168\.\d+\.\d+(:\d+)?$/.test(origin) ||
-/^http:\/\/43\.205\.183\.60(:\d+)?$/.test(origin)
-      ) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: (origin, callback) => {
+    if (
+      !origin ||
+      /^http:\/\/localhost(:\d+)?$/.test(origin) ||
+      /^http:\/\/192\.168\.\d+\.\d+(:\d+)?$/.test(origin) ||
+      /^http:\/\/\d+\.\d+\.\d+\.\d+(:\d+)?$/.test(origin)
+    ) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
 
 app.use(express.json());
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/tasks", require("./routes/taskRoutes"));
+
+app.use(express.static(path.join(__dirname, "dist")));
+app.get("/{*path}", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
